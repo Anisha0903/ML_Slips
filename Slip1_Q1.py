b1c0c48747822ccc09627ba368f22fa22298aa47
Anisha0903 <anishabpeth@gmail.com>
2025-11-19 00:34:24 +0530

    Create Slip1_Q1.py

---
+++ Slip1_Q1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+df = pd.read_csv("groceries.csv", header=None)
+dataset = df.values.tolist()
+
+te = TransactionEncoder()
+te_array = te.fit(dataset).transform(dataset)
+df_encoded = pd.DataFrame(te_array, columns=te.columns_)
+
+frequent_itemsets = apriori(df_encoded, min_support=0.25, use_colnames=True)
+print(frequent_itemsets)
+
+rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.5)
+print(rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']])
